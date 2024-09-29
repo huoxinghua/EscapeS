97c07ae2248e91930eee1b4804bd7ee8635fc888
deepanshu <deepanshu120100@gmail.com>
2024-09-28 22:31:13 -0600

    Update V3
    +fixed animation of player and enemy
    +changed collision method in debug,log its working now but the function is not

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,43 +4,43 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-      [SerializeField] public List<Transform> patrolPoints; 
-      [SerializeField] public float speed = 20f;     
-      [SerializeField] private int currentPointIndex = 0;  
-      [SerializeField] public float pointRadius = 2f;   
-      [SerializeField] private LayerMask smellBomb;
-      
-      private bool _isStopped = false;
-       void Update()
-       {
-           if (!_isStopped && patrolPoints.Count > 0)
-           {
-               Patrolling();
-           }
-       }
-       void Patrolling()
-       {
-           Transform targetPoint = patrolPoints[currentPointIndex];
-           Vector3 direction = targetPoint.position - transform.position;
-           transform.position += direction.normalized * (speed * Time.deltaTime);
+    public List<Transform> patrolPoints;
+    [SerializeField] public float speed = 20f;
+    [SerializeField] private int currentPointIndex = 0;
+    [SerializeField] public float pointRadius = 2f;
 
-           if (Vector3.Distance(transform.position, targetPoint.position) < pointRadius)
-           {
-               currentPointIndex = (currentPointIndex + 1) % patrolPoints.Count;
-           }
-       }
-       void OnTriggerEnter(Collider other)
-       {
-           if (((1 << other.gameObject.layer) & smellBomb) != 0) 
-           {
-               StartCoroutine(HoldCooldown(5f)); 
-           }
-       }
-
-       private IEnumerator HoldCooldown(float duration) 
-       {
-           _isStopped = true; 
-           yield return new WaitForSeconds(duration); 
-           _isStopped = false; 
-       }
+    private bool _isStopped = false;
+    void Update()
+    {
+        if (!_isStopped)
+        {
+            Patrolling();
+        }
+    }
+    void Patrolling()
+    {
+        Transform targetPoint = patrolPoints[currentPointIndex];
+        Vector3 direction = targetPoint.position - transform.position;
+        float distanceToTarget = Vector3.Distance(transform.position, targetPoint.position);
+        float distanceToMove = Mathf.Min(speed * Time.deltaTime, distanceToTarget);
+        
+        transform.position += direction.normalized * distanceToMove;
+        if (distanceToTarget <= pointRadius)
+        {
+            currentPointIndex = (currentPointIndex + 1) % patrolPoints.Count;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("SmellBomb"))
+        {
+            StartCoroutine(HoldCooldown(5f));
+        }
+    }
+    private IEnumerator HoldCooldown(float duration)
+    {
+        _isStopped = true;
+        yield return new WaitForSeconds(duration);
+        _isStopped = false;
+    }
 }
