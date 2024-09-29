253b912f0f70243ca226d33c2b049f481a83f622
deepanshu <deepanshu120100@gmail.com>
2024-09-29 17:33:26 -0600

    Collectables hud
    +it is displaying when in hud when the collectables are collected

--- Assets/Scripts/Collectables.cs
+++ Assets/Scripts/Collectables.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Collectables : MonoBehaviour
 {
+   [SerializeField] public List<GameObject> CollectDisplay;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player"))
@@ -11,6 +12,14 @@ public class Collectables : MonoBehaviour
             GameManager._points += 1;
             Debug.Log(GameManager._points);
             Destroy(gameObject);
+            if (CollectDisplay.Count >= GameManager._points)
+            {
+                GameObject collectDisplay = CollectDisplay[GameManager._points - 1];
+                if (collectDisplay != null && !collectDisplay.activeInHierarchy)
+                {
+                    collectDisplay.SetActive(true); 
+                }
+            }
         }
        
     }
