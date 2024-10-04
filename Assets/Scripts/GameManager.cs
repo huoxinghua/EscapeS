3237ed56df902dd1a74a0bbd4f614447ef2fa04d
deepanshu <deepanshu120100@gmail.com>
2024-10-03 22:04:28 -0600

    Almost done
    +all enemy petrol added
    +all collectables placed
    +all screen changer assigned

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,19 @@
+using System;
 using UnityEngine;
 public class GameManager : MonoBehaviour
 {
   public static int Points = 0;
+  [SerializeField] GameObject gate;
+  [SerializeField] GameObject fgate;
+  private void Update()
+  {
+    if (Points >= 8)
+    {
+      gate.SetActive(false);
+    }
+    if (Points >= 14)
+    {
+      fgate.SetActive(false);
+    }
+  }
 }
